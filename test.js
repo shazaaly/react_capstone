6ff632a5cb4cc22ee692047647d048cc6766d097
shazaAly <shaza.aly@gmail.com>
2023-01-15 17:29:50 +0200

    extensions and set up

---
+++ test.js
@@ -0,0 +1,19 @@
+// const game = {
+//     scored: [
+//         'samuel',
+//         'samuel',
+//         'shaza',
+//         'taha'
+
+//     ]
+// }
+
+// const players = game.scored
+// console.log(players);
+// const myObj = {}
+
+// players.forEach(player => {
+//     myObj[player] = (myObj[player] || 0) + 1
+// })
+
+// console.log(myObj);
